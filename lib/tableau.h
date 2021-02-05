19bd365c018f11a6d7a4bebb1ca76dc0dc011d72
bastien7273 <77972667+bastien7273@users.noreply.github.com>
2021-02-05 15:16:34 +0100

    Add files via upload

--- lib/tableau.h
+++ lib/tableau.h
@@ -2,20 +2,30 @@
 #define _TABLEAU_H_
 
 #define N 4 //la taille du jeu (N cartes)
-#define M 4 //chaque carte est divisé en M partie
+#define M 4 //chaque carte est divisé en M^2 partie
 
 //VARIABLES
 typedef struct zone_s{
-  int element;//type de cette partie(sol,lave,pic,...)
+  char element;//type de cette partie('s'ol, 'l'ave, 'p'ic, 'v'ide)
   //affichage
 }zone_t;
 
-zone_t carte[M][M];//une carte
-void jeu[M];//le jeu
-int ordre[M];//indique comment les cartes sont conectées
+typedef struct carte_s{
+  zone_t * carte[M][M];//une carte
+  int nb_sol;//nombre de sol
+  int nb_pic;//nombre de pic
+  int nb_lave;//nombre de lave
+  int nb_vide;//nombre de vide
+}carte_t;
+
+carte_t * jeu[N];//le jeu
+int ordre[N];//indique comment les cartes sont conectées
 
 //FONCTIONS
-zone_t creation_zone(void);//création d'une zone
-void creation_carte(jeu[M]);//création du jeu
-void ordre_aleatoire(ordre[M]);//cartes mises aléatoirement
+zone_t * creation_zone(void);//création d'une zone
+carte_t * creation_carte(void);//création d'une carte
+void creation_jeu(void);//création du jeu
+int recherche_ordre(int, int);//recherche si r est déja dans ordre jusqu'à l
+void ordre_aleatoire(void);//cartes mises aléatoirement
+
 #endif
