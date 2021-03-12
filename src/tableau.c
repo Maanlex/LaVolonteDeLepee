ec2da6385eb3478f3b4b295adb5d1145a872026c
bastien7273 <77972667+bastien7273@users.noreply.github.com>
2021-03-12 08:47:43 +0100

    Update tableau.c

--- src/tableau.c
+++ src/tableau.c
@@ -6,7 +6,7 @@
 zone_t * creation_zone(int i){
   zone_t * z = malloc(sizeof(zone_t));
   switch(i){
-    case 0 : z->element='s'; z->c='s';break;//affichage a remplir
+    case 0 : z->element='s'; z->c='s'; break;//affichage a remplir
     case 1 : z->element='p'; z->c='p'; break;
     case 2 : z->element='l'; z->c='l'; break;
     case 3 : z->element='v'; z->c='v'; break;
@@ -17,53 +17,42 @@ zone_t * creation_zone(int i){
 carte_t * creation_carte(int n){
   srand(time(NULL));
   int i, j, k;
-  int compt;
   carte_t * c = malloc(sizeof(carte_t));
   c->nb_sol = 0;
   c->nb_pic = 0;
   c->nb_lave = 0;
   c->nb_vide = 0;
   zone_t * z;
-  switch(n){
+  switch(n){//trouver un moyen pour que l'espace du sol soit dynamique
     case 0: //ligne droite
       for(i=0;i<M;i++){
         for(j=0;j<M;j++){
-         if(i<(M/2-1)||i>(M/2+1)){//avant (M/2)-2 et après (M/2)+2 ce n'est que du sol
+         if(i<(2)||i>(M-3)){//avant (M/2)-1 et après (M/2)+1 ce n'est que du sol
          z = creation_zone(0);
          c->nb_sol++;
          }
-         else{
-          if((i==M/2)||((i==M/2-1))){//zone où le joueur n'a que du vide (déplacements)
-            z = creation_zone(4);
+         else{//zone où le joueur n'a que du vide (déplacements)
+            z = creation_zone(3);
             c->nb_vide++;
           }
-          else{//pièges
-            if(i%3==0){
-              z = creation_zone(0);
-              c->nb_sol++;
-            }
-            else{
-              k = rand()%3;
-              z = creation_zone(k);
-              switch(k){
-                case 0: c->nb_sol++;
-                case 1: c->nb_pic++;
-                case 2: c->nb_lave++;
-              }
-            }
-
+        if(i==M-2&&j!=M-1&&j%3!=0){//pièges
+          k = rand()%3;
+          z = creation_zone(k);
+          switch(k){
+            case 0: c->nb_sol++;
+            case 1: c->nb_pic++;
+            case 2: c->nb_lave++;
           }
         }
         c->carte[i][j] = z;
         }
       }
     break;
-    case 1: //monté a corriger
-      compt = 0;
+    case 1: //monté
       for(i=0;i<M;i++){
         for(j=0;j<M;j++){
-         if((i==j&&i==compt)||j==i+1||i==j+1){//zone où le joueur n'a que du vide (déplacements)
-          z = creation_zone(4);
+         if(i==M-1-j||(i==M-j)||(i==M-2-j)){//zone où le joueur n'a que du vide (déplacements)
+          z = creation_zone(3);
           c->nb_vide++;
          }
          else{//ce n'est que du sol en dehors de la diagonale
@@ -72,15 +61,13 @@ carte_t * creation_carte(int n){
         }
         c->carte[i][j] = z;
         }
-        compt++;
       }
     break;
-    case 2: //descente a corriger
-    compt = M-1;
+    case 2: //descente
       for(i=0;i<M;i++){
         for(j=0;j<M;j++){
-         if((i==j&&i==compt)||j==i+1||i==j-1){//zone où le joueur n'a que du vide (déplacements)
-          z = creation_zone(4);
+         if(i==j||i==j+1||i==j-1){//zone où le joueur n'a que du vide (déplacements)
+          z = creation_zone(3);
           c->nb_vide++;
          }
          else{//ce n'est que du sol en dehors de la diagonale
@@ -89,18 +76,30 @@ carte_t * creation_carte(int n){
         }
         c->carte[i][j] = z;
         }
-        compt--;
       }
     break;
     case 3: //boss
-    break;//case a rajouter avec l'apparence de la carte
+      for(i=0;i<M;i++){
+        for(j=0;j<M;j++){
+         if(i<(2)||i>(M-3)){//avant (M/2)-2 et après (M/2)+2 ce n'est que du sol
+         z = creation_zone(0);
+         c->nb_sol++;
+         }
+         else{//zone où le joueur n'a que du vide (déplacements)
+            z = creation_zone(3);
+            c->nb_vide++;
+         }
+        c->carte[i][j] = z;
+        }
+      }
+    break;
   }
   return(c);
 }
 
 int creation_jeu(){
   int i;
-  for(i=0;i<N;i++){//nb aleatoire choix carte
+  for(i=0;i<N;i++){//rangement carte
     jeu[i] = creation_carte(i);
   }
   return 1;
@@ -118,4 +117,17 @@ int destruction_jeu(){
     jeu[k] = NULL;
   }
   return 1;
+}
+
+void afficher_jeu(){
+  int k, i, j;
+  for(k=0;k<N;k++){
+  for(i=0;i<M;i++){
+    for(j=0;j<M;j++){
+      printf("%c", jeu[k]->carte[i][j]->c);
+    }
+    printf("\n");
+  }
+  printf("\n");
+}
 }
